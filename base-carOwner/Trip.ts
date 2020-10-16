224870ff5ec8b50c136747b43ce173156ea6a28b
lgnvpr <lgnvpr@gmail.com>
2020-10-16 08:32:55 +0000

    uupdate

--- base-carOwner/Trip.ts
+++ base-carOwner/Trip.ts
@@ -1,10 +1,12 @@
 import { BaseModel } from "../BaseModel";
 import { Staff } from "./Staff";
 import { Route } from "./Route";
+import { Car } from "./Car";
 
 export class Trip extends BaseModel {
     timeStart ?: Date;
     CarId ?: string;
+    Car ?: Car ;
     driveId ?: string;
     RouteId ?:string;
     Route ? : Route;
