568ca679170b778a91b4bc3265adde1660191565
lgnvpr <lgnvpr@gmail.com>
2020-10-16 08:31:23 +0000

    update module

--- base-carOwner/Ticket.ts
+++ base-carOwner/Ticket.ts
@@ -2,8 +2,13 @@ import {BaseModel} from '../BaseModel';
 import { Staff } from './Staff';
 import { Route } from './Route';
 import { Trip } from './Trip';
+import { ChairCar } from './ChairCar';
+import { Car } from './Car';
 export class Ticket extends BaseModel {
-    carId  ?:string;
+    ChairCarId  ?:string;
+    ChairCar ?: ChairCar;
+    CarId  ?: string;
+    Car ?: Car;
     customerId ?:string;
     customer ?: Staff;
     staffId ?:string;
--- base-carOwner/Trip.ts
+++ base-carOwner/Trip.ts
@@ -1,10 +1,12 @@
 import { BaseModel } from "../BaseModel";
+import { Car } from "./Car";
 import { Route } from "./Route";
 import { Staff } from "./Staff";
 
 export class Trip extends BaseModel {
     timeStart ?: Date;
     CarId ?: string;
+    Car ?: Car ;
     driveId ?: string;
     RouteId ?:string;
     Route ? : Route;
